739844572c1718e1a7bd5797c84a642d8dc17ce7
Joseph Adams <joseph.adams@blueyonder.co.uk>
2022-02-01 16:41:01 +0000

    fixed linting errors

--- src/App.js
+++ src/App.js
@@ -1,68 +1,77 @@
-import React, { useState, useEffect } from 'react';
+/* eslint-disable react/prop-types */
+import React, {useState, useEffect} from 'react';
 import Login from './components/Login';
 import Chatroom from './components/Chatroom';
 import {
   Route,
-  BrowserRouter as Router, 
+  BrowserRouter as Router,
   Switch,
-  Redirect
+  Redirect,
 } from 'react-router-dom';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
 
-function PrivateRoute({ component: Component, authenticated, ...rest}) {
+function PrivateRoute({component: Component, authenticated, ...rest}) {
   return (
-      <Route 
-        {...rest} 
-        render={props => 
+    <Route
+      {...rest}
+      render={(props) =>
           authenticated === true ? (
             <Component {...props} />
           ) : (
-            <Redirect 
-              to={{ pathname: "/", state: {from: props.location} }} 
+            <Redirect
+              to={{pathname: '/', state: {from: props.location}}}
             />
           )
-        } 
-      />
-  )
+      }
+    />
+  );
 }
 
-function PublicRoute({ component: Component, authenticated, ...rest}) {
+function PublicRoute({component: Component, authenticated, ...rest}) {
   return (
-      <Route 
-        {...rest} 
-        render={props => 
+    <Route
+      {...rest}
+      render={(props) =>
           authenticated === false ? (
             <Component {...props} />
           ) : (
             <Redirect to="chatroom" />
           )
-        } 
-      />
-  )
+      }
+    />
+  );
 }
 
 function App() {
   const [authenticated, setAuthenticated] = useState(false);
 
   useEffect(() => {
-    console.log("Authenticated", authenticated)
-    firebase.auth().onAuthStateChanged(user => {
-      console.log(user)
+    console.log('Authenticated', authenticated);
+    firebase.auth().onAuthStateChanged((user) => {
+      console.log(user);
       if (user) {
-        setAuthenticated(true)
+        setAuthenticated(true);
       } else {
-        setAuthenticated(false)
+        setAuthenticated(false);
       }
-    })
+    });
   }, []);
 
   return (
     <div>
       <Router>
         <Switch>
-          <PublicRoute exact path='/' authenticated={authenticated} component={Login} />
-          <PrivateRoute exact path='/chatroom' authenticated={authenticated} component={Chatroom} />
+          <PublicRoute
+            exact path='/'
+            authenticated={authenticated}
+            component={Login}
+          />
+          <PrivateRoute
+            exact path='/chatroom'
+            authenticated={authenticated}
+            component={Chatroom}
+          />
         </Switch>
       </Router>
     </div>

37c4d0359c464af02210ff2faeb550c1ab8cec4c
Joseph Adams <joseph.adams@blueyonder.co.uk>
2022-02-01 17:05:59 +0000

    placeholder messages

--- src/components/Chatroom.js
+++ src/components/Chatroom.js
@@ -22,7 +22,54 @@ function Chatroom() {
 
   return (
     <div>
-      <h1>Chatroom</h1>
+      <div style={{
+        display: 'flex',
+        flex: 1,
+        height: '100vh',
+        flexDirection: 'column'}}>
+        <div style={{
+          flex: 1,
+          marginLeft: 24,
+          marginRight: 24,
+          overflow: 'auto',
+          marginBottom: 24}}>
+          <div style={{display: 'flex', flex: 1, justifyContent: 'flex-start'}}>
+            <div style={{
+              minHeight: 52,
+              width: 600,
+              backgroundColor: 'red',
+              marginTop: 24,
+              marginLeft: 24,
+              marginRight: 24,
+              borderRadius: 12}}>
+              <p>Message from someone else</p>
+            </div>
+          </div>
+          <div style={{display: 'flex', flex: 1, justifyContent: 'flex-end'}}>
+            <div style={{
+              minHeight: 52,
+              width: 600,
+              backgroundColor: 'blue',
+              marginTop: 24,
+              marginLeft: 24,
+              marginRight: 24,
+              borderRadius: 12}}>
+              <p>Message from me</p>
+            </div>
+          </div>
+        </div>
+        <div stype={{display: 'flex', flexDirection: 'row', marginTop: 24}}>
+          <input style={{flex: 11, height: 32, fontSize: 28}}/>
+          <button style={{
+            flex: 1,
+            backgroundColor: 'blue',
+            colour: 'white',
+            fontWeight: 'bold',
+            fontSize: 18,
+            borderWidth: 0,
+          }}>Send</button>
+        </div>
+      </div>
     </div>
   );
 }

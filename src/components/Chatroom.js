70efeba86314b0ea7e5c45c4862e4215c2a4ea46
Joseph Adams <joseph.adams@blueyonder.co.uk>
2022-02-01 16:40:53 +0000

    updated with: WIP chatroom Fixed linting errors

--- src/components/Chatroom.js
+++ src/components/Chatroom.js
@@ -1,9 +1,21 @@
+import React, {useEffect} from 'react';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+
 function Chatroom() {
-    return (
-        <div>
-            <h1>Chatroom</h1>
-        </div>
-    )
+  const firestore = firebase.firestore();
+  useEffect(async () => {
+    const snapshot = await firestore.collection(`chats`).get();
+    snapshot.forEach((doc) => {
+      console.log(doc.id, '=>', doc.data());
+    });
+  }, []);
+
+  return (
+    <div>
+      <h1>Chatroom</h1>
+    </div>
+  );
 }
 
 export default Chatroom;
